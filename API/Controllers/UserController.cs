a36fc7fd2f624ca97bb77db6e4fe1fc4a9cc8ee9
pravash1990 <71909237+pravash1990@users.noreply.github.com>
2021-10-24 21:25:36 +0530

    Initial commit

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>>GetUser ()
+        {
+            return  await _context.Users.ToListAsync();
+            
+        }
+
+        [HttpGet("{id}")]
+        public  async Task<ActionResult<AppUser>> GetUser (int id)
+        {
+            return await  _context.Users.FindAsync(id);
+        }
+
+    }
+}
